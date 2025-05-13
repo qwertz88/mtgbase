8a4da7424d2e27109f2037c2965758ef50e4514b
Thomas <thomas.meersschaut@gmail.com>
2025-05-13 21:30:46 +0200

    Add Deck class to represent player-specific decks
    This class initializes with deck ID, deck name, and player ID. It will serve as a foundational component for managing player decks in the application.

---
+++ mgtbase/deck.py
@@ -0,0 +1,11 @@
+
+class Deck:
+    def __init__(
+            self,
+            deck_id: int,
+            deck_name: str,
+            player_id: int,
+    ):
+        self.deck_id = deck_id
+        self.deck_name = deck_name
+        self.player_id = player_id
