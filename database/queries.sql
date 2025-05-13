8c711ecacf0c47e548351c285b2e58dab2aeacc7
Synix1997 <deruyck.sebastien97@gmail.com>
2025-05-13 19:37:05 +0200

    Fix SQL query by replacing placeholder <your_deck_id> with actual value 1 for testing

--- database/queries.sql
+++ database/queries.sql
@@ -62,7 +62,7 @@ from
     join tbl_deck_cards dc on d.deck_id = dc.deck_id
     join tbl_card_prices cp on dc.card_id = cp.card_id
 where
-    d.deck_id = <your_deck_id>
+    d.deck_id = 1
 group by
     d.deck_id, d.deck_name;
 
