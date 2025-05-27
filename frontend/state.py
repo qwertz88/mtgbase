f512f6827f3e9779905a8e798cda0734f01f1af9
Freekvera <veraghtertfreek@gmail.com>
2025-05-27 19:18:40 +0200

    Add frontend

--- frontend/state.py
+++
@@ -1,6 +0,0 @@
-from shiny import reactive
-
-session_user = reactive.Value(None)       # Who's logged in
-ui_mode = reactive.Value("login")         # "login" or "register"
-active_deck = reactive.Value(None)        # Name of selected/open deck
-card_update_counter = reactive.Value(0)  # 💬 Forces card list refresh
